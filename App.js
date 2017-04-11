9cb033b681b6140713ad3128f645e981dce4c4f8
ITentity <zzx5888@qq.com>
2017-04-11 10:32:00 +0800

    complete

--- App.js
+++ App.js
@@ -9,12 +9,20 @@ import {
   View,
   Text,
 } from 'react-native';
+import { startWith, endWith } from './flow';
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Text>111</Text>
+        <Text>
+          111
+          {console.log(startWith('W06XADFA', 'W06X'))}
+          {console.log(startWith('W06X_ADFA', 'W06X'))}
+          {console.log(startWith('W06ADFA', 'W06X'))}
+          {console.log(endWith('adfasdW06X', 'W06X'))}
+          {console.log(endWith('adfasdW06', 'W06X'))}
+        </Text>
       </View>
     );
   }
--- flow.js
+++ flow.js
@@ -6,3 +6,14 @@
  export function trim(s: string) {
    return s.replace(/(^\s+)|(\s+$)/g, '');
  }
+
+// 字符串s1以s2开头
+ export function startWith(s1: string, s2: string) {
+   const reg = new RegExp(`^${s2}`);
+   return reg.test(s1);
+ }
+ // 字符串s1以s2结尾
+ export function endWith(s1: string, s2: string) {
+   const reg = new RegExp(`${s2}$`);
+   return reg.test(s1);
+ }
