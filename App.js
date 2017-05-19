05f9c98f5723720e4a786f7c51509aa4edf3f663
ITentity <zzx5888@qq.com>
2017-05-19 14:52:33 +0800

    验证网络

--- App.js
+++ App.js
@@ -10,14 +10,32 @@ import {
   Text,
   TouchableWithoutFeedback,
 } from 'react-native';
-import { add } from './flow';
 
 export default class App extends Component {
+  // 注意这个方法前面有async关键字
+  async netWork() {
+    alert(111);
+    try {
+       // 注意这里的await语句，其所在的函数必须有async关键字声明
+      const response = await fetch('https://facebook.github.io/react-native/movies.json');
+      const responseJson = await response.json();
+      console.log(`获取百度的内容1${responseJson}`);
+      console.log(`获取百度的内容2${JSON.stringify(responseJson)}`);
+      return responseJson.movies;
+    } catch (error) {
+      console.log(`获取百度的内容3${error}`);
+      console.error(error);
+      return error;
+    }
+  }
+
   render() {
     return (
       <View>
-        <TouchableWithoutFeedback onPress={() => alert(add(a, b))}>
-          <Text>111</Text>
+        <TouchableWithoutFeedback onPress={() => this.netWork()}>
+          <View style={{ marginTop: 30 }}>
+            <Text>111</Text>
+          </View>
         </TouchableWithoutFeedback>
       </View>
     );
