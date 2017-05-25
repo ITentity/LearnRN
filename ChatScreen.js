4a0c8d3b4f6f1df6b54c63671e73296e284c034e
ITentity <zzx5888@qq.com>
2017-05-25 11:07:00 +0800

    增加参数的navigator

--- ChatScreen.js
+++ ChatScreen.js
@@ -11,13 +11,15 @@ import {
 } from 'react-native';
 
 export default class ChatScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Chat with Lucy',
-  };
+
+  static navigationOptions = ({ navigation }) => ({
+    title: `Chat with ${navigation.state.params.user}`,
+  });
   render() {
+    const { params } = this.props.navigation.state;
     return (
       <View>
-        <Text>Chat with Lucy</Text>
+        <Text>Chat with {params.user}</Text>
       </View>
     );
   }
--- HomeScreen.js
+++ HomeScreen.js
@@ -18,8 +18,8 @@ class HomeScreen extends React.Component {
       <View>
         <Text>Hello, Chat App!</Text>
         <Button
-          onPress={() => navigate('Chat')}
-          title="Chat with Lucy"
+          onPress={() => navigate('Chat', { user: 'zhaozx' })}
+          title="Chat with zhaozx"
         />
       </View>
     );
