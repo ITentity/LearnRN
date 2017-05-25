98c3d17ae8bbf106d235b1605367cf1780d62adb
ITentity <zzx5888@qq.com>
2017-05-25 15:46:18 +0800

    使用navigator属性成功

--- HomeScreen.js
+++ HomeScreen.js
@@ -9,18 +9,47 @@ import AllContactsScreen from './AllContactsScreen';
 import ChatScreen from './ChatScreen';
 
 const MainScreenNavigator = TabNavigator({
-  zhaozx: { screen: RecentChatsScreen },
-  bingo: { screen: AllContactsScreen },
-});
+  zhaozx: {
+    screen: RecentChatsScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'zhaozx chat',
+    }),
+  },
+  bingo: {
+    screen: AllContactsScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'bingo chat',
+    }),
+  },
+},
+  {
+    tabBarOptions: {
+      activeTintColor: '#e91e63',
+    },
+  },
+);
 
 // Because MainScreenNavigator is being used as a screen, we can give it navigationOptions
-MainScreenNavigator.navigationOptions = {
-  title: 'My Chats',
-};
+// MainScreenNavigator.navigationOptions = {
+//   title: 'My Chats',
+// };
 
 const SimpleApp = StackNavigator({
-  Home: { screen: MainScreenNavigator },      // 第一个就是显示出来的tab页面
-  Chat: { screen: ChatScreen },
-});
+  Home: {
+    screen: MainScreenNavigator,
+  },      // 第一个就是显示出来的页面
+  Chat: {
+    screen: ChatScreen,
+  },
+},
+  {
+    mode: 'modal',
+    navigationOptions: {
+      headerTruncatedBackTitle: '',
+      headerTintColor: '#f00',
+    },
+  },
+);
+
 
 AppRegistry.registerComponent('LearnRN', () => SimpleApp);
