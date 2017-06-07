9b9aa91118c33f1917c71b5cc385a7d3923ba9f5
ITentity <zzx5888@qq.com>
2017-06-07 09:27:54 +0800

    完成JNI的hello world

--- android/app/src/main/java/zhaozx/JniTest.java
+++ android/app/src/main/java/zhaozx/JniTest.java
@@ -8,14 +8,13 @@ package zhaozx;
 public class JniTest {
     /**
      * 将用C++代码实现，在android代码中调用的方法：获取当前app的包名
-     * @param o
      * @return
      */
     public static native String getPackname();
 
     /**
      * 加载so库或jni库，在使用到该库之前加载就行，不一定非要写在这个类内
-     * 系统自己会判断扩展名是dll还是so,这里加载libJNI_ANDROID_TEST.so
+     * 系统自己会判断扩展名是dll还是so,这里加载libapp.so
      */
     static {
         System.loadLibrary("app");
