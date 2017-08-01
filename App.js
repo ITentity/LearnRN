03c84ba505421abd7ad589b6f1d7aa2b879ad6b3
ITentity <zzx5888@qq.com>
2017-08-01 15:47:12 +0800

    尝试使用下拉刷新

--- App.js
+++ App.js
@@ -9,16 +9,59 @@ import {
   View,
   Text,
   TouchableWithoutFeedback,
+  ListView,
+  RefreshControl,
+  ActivityIndicator,
+  Spinner,
 } from 'react-native';
-import { add } from './flow';
 
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 export default class App extends Component {
+
+  // 构造
+  constructor(props) {
+    super(props);
+      // 初始状态
+    this.state = {
+      dataSource: ds.cloneWithRows([1,2,3,4,56,7,89,0,12,23,34,45,56]),
+      refreshing: false,
+    };
+  }
+
+  componentWillUnmount() {
+    this.timeOut && clearTimeout(this.timeOut);
+  }
+
+  closeRefresh() {
+    this.setState({ refreshing: true });
+    // this.timeOut = setTimeout(()=>{
+    //   alert('停止刷新');
+    //   this.setState({
+    //     refreshing: false,
+    //   });
+    // }, 1000)
+  }
+
   render() {
     return (
       <View>
-        <TouchableWithoutFeedback onPress={() => alert(add(a, b))}>
-          <Text>111</Text>
-        </TouchableWithoutFeedback>
+        <ListView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={() => { () => this.closeRefresh(); }}
+              colors={['#ff0000', '#00ff00','#0000ff','#3ad564']}
+              progressBackgroundColor="#ffffff"
+            />}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => {
+            return (
+              <View>
+                <Text>{rowData}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
     );
   }
