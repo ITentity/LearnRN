049bcd2e599e70a26f9e81f74a820afa8fd3f926
ITentity <zzx5888@qq.com>
2017-08-07 17:49:25 +0800

    测试定时器在应用切到后台后是否还在运行

--- App.js
+++ App.js
@@ -13,12 +13,55 @@ import {
 import { add } from './flow';
 
 export default class App extends Component {
+  // 构造
+  constructor(props) {
+    super(props);
+    // 初始状态
+    this.state = {
+      showTime: 0,
+      isRun: false,
+    };
+  }
+
+  renderButton() {
+    if(this.state.isRun){
+      return(
+        <TouchableWithoutFeedback onPress={() => this.endTime()}>
+          <Text>停止</Text>
+        </TouchableWithoutFeedback>
+      );
+    }
+    return(
+      <TouchableWithoutFeedback onPress={() => this.startTime()}>
+        <Text>开始</Text>
+      </TouchableWithoutFeedback>
+    );
+  }
+
+  startTime() {
+    let i = 0
+    this.timer = setInterval(()=>{
+      i += 1;
+      this.setState({
+        showTime: i,
+        isRun: true,
+      })
+    },1000)
+  }
+
+  endTime() {
+    this.timer&&clearTimeout(this.timer);
+    this.setState({
+      isRun: false,
+      showTime: 0,
+    })
+  }
+
   render() {
     return (
       <View>
-        <TouchableWithoutFeedback onPress={() => alert(add(a, b))}>
-          <Text>111</Text>
-        </TouchableWithoutFeedback>
+        <Text>{this.state.showTime}</Text>
+        {this.renderButton()}
       </View>
     );
   }
